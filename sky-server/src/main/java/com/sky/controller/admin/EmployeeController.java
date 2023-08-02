f5bc861e424aa61ff93ed5433b741bbaaabc040e
ymy2001 <2360302452@qq.com>
2023-08-02 17:52:56 +0800

    启用禁用员工账号

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -97,4 +97,13 @@ public class EmployeeController {
         return Result.success(pageResult);
     }
 
+    /*
+    * 启用禁用员工账号*/
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    public Result ob(@PathVariable Integer status,Long id){
+
+        return Result.success();
+    }
+
 }
